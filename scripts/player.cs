4793103f916a22b803bd7892ad461949c48b7930
lubmir2k <lubomir.mitana@gmail.com>
2021-04-03 16:49:58 +0200

    Added Game over screen

--- scripts/player.cs
+++ scripts/player.cs
@@ -6,6 +6,7 @@ public class player : Node
     bulletBrain bulletBrain;
 
     public bool canShoot = true;
+    public bool gameOver = false;
 
     public int health = 3;
     public int score = 0;
@@ -15,6 +16,14 @@ public class player : Node
         bulletBrain = (bulletBrain)GetNode("/root/game/bullets/bulletBrain");
 
         updateUI();
+    }
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        if (inputEvent.IsActionPressed("click") && (gameOver == true))
+        {
+            GetTree().ReloadCurrentScene();
+        }
     }
 
     public void _on_playerHitZone_area_entered(Area2D bullet)
@@ -32,6 +41,18 @@ public class player : Node
     {
         health = Math.Max(health - damageAmount, 0);
         updateUI();
+
+        if((health <= 0) && (gameOver != true))
+        {
+            gameOver = true;
+            canShoot = false;
+            var gameOverScreen = (Node2D)GetNode("/root/game/hud/gameOverScreen");
+            gameOverScreen.Visible = true;
+
+            var cannon = (Node2D)GetNode("/root/game/foreground/cannon");
+            bulletBrain.spawnExplosion(cannon.GlobalPosition, "enemy");
+            cannon.QueueFree();
+        }
     }
 
     public void addScore(int scoreAmount = 1)
